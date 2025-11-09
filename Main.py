3039926e7e5c247ce4b2c18cd532f14d3aca168c
Ama <amarillis.dk@gmail.com>
2025-11-09 22:56:50 +0100

    Add chatgpt integration and first test message

--- Main.py
+++ Main.py
@@ -1,4 +1,18 @@
 import tkinter as tk
+from openai import OpenAI
+import os
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+response = client.chat.completions.create(
+    model="gpt-4.1-mini", 
+    messages= [
+        {"role":"system", "content":"You are a Dungeons and Dragons 2024 Dungeon Master"}, 
+        {"role":"user", "content": "Generate me a lvl1 wizard statblock" }
+    ], 
+    temperature=0.7
+)
+print(response.choices[0].message.content)
+
 root = tk.Tk()
 frame = tk.Frame(master=root, width=200, height=100)
 frame.pack()
